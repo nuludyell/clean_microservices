b48648a3b474a8b2941e3990ffe8dd7ad656c652
Ariel Nulud <anulud@doehle-mnl.com>
2023-10-24 11:24:54 +0800

    CQRS

---
+++ Eshopping/Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers;
+
+public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+{
+    private readonly IBrandRepository _brandRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllBrandsHandler(
+        IBrandRepository brandRepository,
+        IMapper mapper)
+    {
+        _brandRepository = brandRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        var brandList = await _brandRepository.GetAllBrandsAsync();
+        var brandResponseList = _mapper.Map<IList<BrandResponse>>(brandList);
+
+        return brandResponseList;
+    }
+}
---
+++ Eshopping/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,94 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories;
+
+public class ProductRepository : IProductRepository, IBrandRepository, ITypesRepository
+{
+    private readonly ICatalogContext _context;
+
+    public ProductRepository(ICatalogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        return await _context
+            .Products
+            .Find(p => true)
+            .ToListAsync();
+    }
+
+    public async Task<Product> GetProductAsync(string id)
+    {
+        return await _context
+            .Products
+            .Find(p => p.Id == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsByName(string name)
+    {
+        FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+        return await _context
+            .Products
+            .Find(filter)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsByBrand(string name)
+    {
+        FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Brands.Name, name);
+
+        return await _context
+            .Products
+            .Find(filter)
+            .ToListAsync();
+    }
+
+    public async Task<Product> CreateProductAsync(Product product)
+    {
+        await _context.Products.InsertOneAsync(product);
+
+        return product;
+    }
+
+    public async Task<bool> UpdateProductAsync(Product product)
+    {
+        var updateResult = await _context
+            .Products
+            .ReplaceOneAsync(p => p.Id == product.Id, product);
+
+        return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+    }
+
+    public async Task<bool> DeleteProductAsync(string id)
+    {
+        FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+        DeleteResult deleteResult = await _context
+            .Products
+            .DeleteOneAsync(filter);
+
+        return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+    }
+
+    public async Task<IEnumerable<ProductBrand>> GetAllBrandsAsync()
+    {
+        return await _context
+            .Brands
+            .Find(b => true)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<ProductType>> GetAllTypesAsync()
+    {
+        return await _context
+            .Types
+            .Find(t => true)
+            .ToListAsync();
+    }
+}
